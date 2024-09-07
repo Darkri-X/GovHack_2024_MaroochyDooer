8fa2573645c4a6b56ac1655e0379c1c86980d7be
jettm-github <jettmay04@gmail.com>
2024-09-07 14:02:03 +1000

    Changing the app README.md

---
+++ src/app/README.md
@@ -0,0 +1,17 @@
+# WebPage for OnlyInfo
+## Features of the Page
+### Soft white coloured backgroup
+The backgroup of the page is soft white.
+
+
+### Text input box
+The text inpput box is located in the centre of the page along the bottom. It is a light grey that has a base text of 'Ask a question'. The corners of the text input box are rounded. 
+
+
+### Text response Area
+From the top of the text input box to the bottom of the drop down menu. This area will display the response from OnlyInfo in a messenger style conversation.
+
+
+### Drop down menu
+The Drop down menu is located along to top of the page. It automatically displays the current option that is selected, This will initiall be 'All'. Once clicked the menu will drop down and display the following options: UnityWater, Sunshine Coast Council, Moreton Bay Council. Once another option is selected the menu will now display the new option.
+
---
+++ src/app/app.js
@@ -0,0 +1,41 @@
+  // app.js
+
+  // Import necessary modules
+  import { createApp } from 'vue';
+  import App from './App.vue';
+  import router from './router';
+  import store from './store';
+
+  // Create Vue app instance
+  const app = createApp(App);
+
+  // Use router and store
+  app.use(router);
+  app.use(store);
+
+  // Mount the app to the DOM
+  app.mount('#app');
+
+  // Global error handling
+  app.config.errorHandler = (err, vm, info) => {
+    console.error('Global error:', err);
+    console.error('Vue instance:', vm);
+    console.error('Error info:', info);
+  };
+
+  // Custom directive example
+  app.directive('focus', {
+    mounted(el) {
+      el.focus();
+    }
+  });
+
+  // Global mixin example
+  app.mixin({
+    created() {
+      console.log('Global mixin - component created');
+    }
+  });
+
+  // Export the app instance for potential use in other files
+  export default app;
